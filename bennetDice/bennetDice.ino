53be42a6e401f127a78df75f9f01b19f911e27ef
Sascha-Matthias Kulawik <sascha@kulawik.de>
2014-05-14 23:25:38 +0200

    Update to ATTiny84

--- bennetDice/bennetDice.ino
+++ bennetDice/bennetDice.ino
@@ -1,4 +1,4 @@
-/*
+/* 24
      6
     ---
  1 |   | 5
@@ -8,9 +8,9 @@
      2  x
 */
 
-byte pinLeds1 = 0;
-byte pinLeds2 = 1;
-byte pinLeds3 = 3;
+byte pinLed1 = 0;
+byte pinLed2 = 1;
+byte pinLed3 = 3;
 byte pinLed4 = 8;
 byte pinLed5 = 9;
 byte pinLed6 = 10;
@@ -19,9 +19,9 @@ byte buttonPin = 2;
 
 void setup ()
 {
-  pinMode (pinLeds1, OUTPUT);
-  pinMode (pinLeds2, OUTPUT);
-  pinMode (pinLeds3, OUTPUT);
+  pinMode (pinLed1, OUTPUT);
+  pinMode (pinLed2, OUTPUT);
+  pinMode (pinLed3, OUTPUT);
   pinMode (pinLed4, OUTPUT);
   pinMode (pinLed5, OUTPUT);
   pinMode (pinLed6, OUTPUT);
@@ -31,8 +31,31 @@ void setup ()
 }
 
 void loop() {
+ 
   short buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH){
+    for(int i = 1; i < 6; i++) {
+      int dd = 50 + (20 * i);
+      digitalWrite (pinLed7, LOW);
+      digitalWrite (pinLed1, HIGH);
+      delay (dd);
+       digitalWrite (pinLed1, LOW);
+      digitalWrite (pinLed6, HIGH);
+      delay (dd);
+       digitalWrite (pinLed6, LOW);
+      digitalWrite (pinLed5, HIGH);
+      delay (dd);
+       digitalWrite (pinLed5, LOW);
+      digitalWrite (pinLed4, HIGH);
+      delay (dd);
+       digitalWrite (pinLed4, LOW);
+       digitalWrite (pinLed2, HIGH);
+      delay (dd);       
+       digitalWrite (pinLed2, LOW);
+       digitalWrite (pinLed7, HIGH);
+      delay (dd);
+    }
+    digitalWrite (pinLed7, LOW);
     byte ran = random(1, 7);    
     switch(ran) {
       case 1:
@@ -45,53 +68,53 @@ void loop() {
       {
       digitalWrite (pinLed6, HIGH);
       digitalWrite (pinLed5, HIGH);
-      digitalWrite (pinLeds3, HIGH);
+      digitalWrite (pinLed3, HIGH);
       digitalWrite (pinLed7, HIGH);
-      digitalWrite (pinLeds2, HIGH);
+      digitalWrite (pinLed2, HIGH);
             break;
       }
       case 3:
       {
       digitalWrite (pinLed6, HIGH);
       digitalWrite (pinLed5, HIGH);
-      digitalWrite (pinLeds3, HIGH);
+      digitalWrite (pinLed3, HIGH);
       digitalWrite (pinLed4, HIGH);
-      digitalWrite (pinLeds2, HIGH);
+      digitalWrite (pinLed2, HIGH);
       break;
       }
       case 4:
       {
-      digitalWrite (pinLeds1, HIGH);
+      digitalWrite (pinLed1, HIGH);
       digitalWrite (pinLed5, HIGH);
-      digitalWrite (pinLeds3, HIGH);
+      digitalWrite (pinLed3, HIGH);
       digitalWrite (pinLed4, HIGH);
             break;
       }
       case 5:
       {
       digitalWrite (pinLed6, HIGH);
-      digitalWrite (pinLeds1, HIGH);
-      digitalWrite (pinLeds3, HIGH);
+      digitalWrite (pinLed1, HIGH);
+      digitalWrite (pinLed3, HIGH);
       digitalWrite (pinLed4, HIGH);
-      digitalWrite (pinLeds2, HIGH);
+      digitalWrite (pinLed2, HIGH);
             break;
       }
       case 6:
       {
       digitalWrite (pinLed6, HIGH);
-      digitalWrite (pinLeds1, HIGH);
-      digitalWrite (pinLeds3, HIGH);
+      digitalWrite (pinLed1, HIGH);
+      digitalWrite (pinLed3, HIGH);
       digitalWrite (pinLed4, HIGH);
-      digitalWrite (pinLeds2, HIGH);
+      digitalWrite (pinLed2, HIGH);
       digitalWrite (pinLed7, HIGH);
       }
     }
     
     delay (2000);
   }
-  digitalWrite (pinLeds1, LOW);
-  digitalWrite (pinLeds2, LOW);
-  digitalWrite (pinLeds3, LOW);
+  digitalWrite (pinLed1, LOW);
+  digitalWrite (pinLed2, LOW);
+  digitalWrite (pinLed3, LOW);
   digitalWrite (pinLed4, LOW);
   digitalWrite (pinLed5, LOW);
   digitalWrite (pinLed6, LOW);
